b27b71b6bde3733d62140d550429fb45202f1179
Naoki Yumura <naokiyumura@NaokinoMacBook-ea.local>
2018-02-06 11:45:10 +0900

    before modify model

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,12 +3,12 @@ class RoomsController < ApplicationController
   end
 
   def create
+=begin
     room = Room.new(access_id: params[:access_id])
-    logger.debug(params[:access_id])
     if room.save
-      logger.debug("トーク作成")
       redirect_to room_path(access_id: params[:access_id])
     end
+=end
   end
 
   def destroy
